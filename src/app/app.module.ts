d1c11773ca1ce049800e2bc74cef47acd03d6f27
Ana Clara C. <100969119+Clarinhaaa@users.noreply.github.com>
2025-03-19 13:31:47 -0300

    HTML e CSS do formulário pronto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from 'src/pages/home/home.component';
@@ -22,7 +23,7 @@ import { VisualizarContaComponent } from 'src/pages/visualizar-conta/visualizar-
     EditarContaComponent,
     VisualizarContaComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/components/form-conta/form-conta.component.ts
+++ src/components/form-conta/form-conta.component.ts
@@ -1,10 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-conta',
   templateUrl: './form-conta.component.html',
-  styleUrls: ['./form-conta.component.css']
+  styleUrls: ['./form-conta.component.css'],
 })
-export class FormContaComponent {
+export class FormContaComponent implements OnInit {
   @Input() tipoForm!: string;
+  @Input() visualizar!: boolean;
+
+  protected contaForm!: FormGroup;
+  protected dataAtual: Date = new Date();
+
+  ngOnInit(): void {
+    console.log(this.tipoForm);
+    this.contaForm = new FormGroup({
+      idConta: new FormControl(null, []),
+      descricaoConta: new FormControl({value: null, disabled: this.visualizar}, [Validators.required]),
+      valorConta: new FormControl({value: null, disabled: this.visualizar}, [Validators.required]),
+      dataVencimentoConta: new FormControl({value: null, disabled: this.visualizar}, [Validators.required]),
+      dataPagamentoConta: new FormControl(null, []),
+      tipoConta: new FormControl({value: null, disabled: this.visualizar}, [Validators.required]),
+      statusConta: new FormControl(null, []),
+      idUsuario: new FormControl(null, []),
+      idCategoria: new FormControl({value: null, disabled: this.visualizar}, []),
+    })
+  }
+
+  get descricao() {
+    return this.contaForm.get('descricaoConta')!;
+  }
+  get valor() {
+    return this.contaForm.get('valorConta')!;
+  }
+  get dataVencimento() {
+    return this.contaForm.get('dataVencimentoConta')!;
+  }
+  get tipo() {
+    return this.contaForm.get('tipoConta')!;
+  }
 }
